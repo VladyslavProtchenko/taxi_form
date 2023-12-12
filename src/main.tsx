31b7cbd495c1767d5286f6d8660868a65e86560b
Ihor Kachynskyi <ikachynskiy@gmail.com>
2023-12-12 15:08:53 +0200

    new commit

--- src/main.tsx
+++ src/main.tsx
@@ -11,17 +11,6 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from 'react-query'
-import Dashboard from './pages/Dashboard/Dashboard';
-import Calendar from './pages/Dashboard/Calendar.tsx';
-import Customers from './pages/Dashboard/Customers.tsx';
-import Orders from './pages/Dashboard/Orders.tsx';
-import Drivers from './pages/Dashboard/Drivers.tsx';
-import Prices from './pages/Dashboard/Prices.tsx';
-import Settings from './pages/Dashboard/Settings.tsx';
-import Confirm from './pages/Dashboard/Links/Confirm.tsx';
-import Cancel from './pages/Dashboard/Links/Cancel.tsx';
-import Pending from './pages/Dashboard/Links/Pending.tsx';
-import Edit from './pages/Dashboard/Links/Edit.tsx';
 
 const queryClient = new QueryClient()
 
@@ -29,53 +18,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-    children:[
-      {
-        path: "calendar",
-        element: <Calendar />,
-      },
-      {
-        path: "Customers",
-        element: <Customers />,
-      },
-
-      {
-        path: "orders",
-        element: <Orders />,
-      },
-      {
-        path: "drivers",
-        element: <Drivers />,
-      },
-      {
-        path: "prices",
-        element: <Prices />,
-      },
-      {
-        path: "settings",
-        element: <Settings />,
-      },
-    ]
-  },
-  {
-    path: "/confirm/:id",
-    element: <Confirm />,
-  },
-  {
-    path: "/cancel/:id",
-    element: <Cancel />,
-  },
-  {
-    path: "/pending/:id",
-    element: <Pending />,
-  },
-  {
-    path: "/editOrder/:id",
-    element: <Edit />,
   },
 ]);
 
