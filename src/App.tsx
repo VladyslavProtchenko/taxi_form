3af63a38f1321413bba6fdfd284acbe511554aa2
Ihor Kachynskyi <ikachynskiy@gmail.com>
2023-12-28 12:31:01 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -81,37 +81,39 @@ function App():React.ReactNode {
         </div>
 
         <div className={footer}>
-          <span className={list[activeCarId-1].steps===1?footerTabActive: footerTab} onClick={()=>{setSubmit(false); setSteps(1)}}>
-            <PiUserListLight className={footerIcon}/>
-            <span className={footerTabText}>Info</span>
-          </span>
-          <span className={list[activeCarId-1].steps===2?footerTabActive: footerTab} onClick={()=>{
-            setSubmit(false);
-              if(list[activeCarId-1].validation>0) setSteps(2)
+          <div className={footerWrapper}>
+            <span className={list[activeCarId-1].steps===1?footerTabActive: footerTab} onClick={()=>{setSubmit(false); setSteps(1)}}>
+              <PiUserListLight className={footerIcon}/>
+              <span className={footerTabText}>Info</span>
+            </span>
+            <span className={list[activeCarId-1].steps===2?footerTabActive: footerTab} onClick={()=>{
+              setSubmit(false);
+                if(list[activeCarId-1].validation>0) setSteps(2)
+              }}>
+              <CiLocationOn className={footerIcon} />
+              <span className={footerTabText}>Locations</span>
+            </span>
+            <span className={list[activeCarId-1].steps===3?footerTabActive: footerTab} onClick={()=>{
+              setSubmit(false);
+              if(list[activeCarId-1].validation>1) setSteps(3)
             }}>
-            <CiLocationOn className={footerIcon} />
-            <span className={footerTabText}>Locations</span>
-          </span>
-          <span className={list[activeCarId-1].steps===3?footerTabActive: footerTab} onClick={()=>{
-            setSubmit(false);
-            if(list[activeCarId-1].validation>1) setSteps(3)
-          }}>
-            <CiBoxList  className={footerIcon}/>
-            <span className={footerTabText}>Options</span>
-          </span>
-          <span className={list[activeCarId-1].steps===4?footerTabActive: footerTab} onClick={()=>{
-            setSubmit(false);
-            if(list[activeCarId-1].validation>1) setSteps(4)
-          }}>
-            <CiMoneyCheck1 className={footerIcon} />
-            <span className={footerTabText}>Payment</span>
-          </span>
-          <span className={submit?footerTabActive: footerTab} onClick={()=>{
-              setSubmit(true)
-          }}>
-            <IoCheckmarkDone className={footerIcon} />
-            <span className={footerTabText}>Conirm</span>
-          </span>
+              <CiBoxList  className={footerIcon}/>
+              <span className={footerTabText}>Options</span>
+            </span>
+            <span className={list[activeCarId-1].steps===4?footerTabActive: footerTab} onClick={()=>{
+              setSubmit(false);
+              if(list[activeCarId-1].validation>1) setSteps(4)
+            }}>
+              <CiMoneyCheck1 className={footerIcon} />
+              <span className={footerTabText}>Payment</span>
+            </span>
+            <span className={submit?footerTabActive: footerTab} onClick={()=>{
+                setSubmit(true)
+            }}>
+              <IoCheckmarkDone className={footerIcon} />
+              <span className={footerTabText}>Conirm</span>
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -119,6 +121,7 @@ function App():React.ReactNode {
 }
 export default App
 
+const footerWrapper = 'flex max-w-[768px] w-full px-3 py-2 flex justify-around'
 const footerIcon = 'text-lg'
 const footerTabText = 'text-[10px]'
 const footerTab = 'flex flex-col items-center  px-3 py-1 text-gray-500 cursor-pointer'
@@ -131,7 +134,7 @@ const tabsContainer = 'bg-white flex mt-2 items-center text-xs mb-0 shadow-lg ro
 const lang = 'flex cursor-pointer items-center  mt-8 mb-2 '
 const langItem = ' px-1 text-gray-600 font-thin'
 
-const footer = 'flex justify-around border-t bg-white px-3 py-2 fixed bottom-0 left-0 right-0 z-20 '
+const footer =  'flex justify-center border-t bg-white  fixed bottom-0 left-0 right-0 z-20 '
 const content =  'flex flex-1 w-full justify-center pb-[60px]'
 const header = 'flex flex-col px-10 w-full  '
 
